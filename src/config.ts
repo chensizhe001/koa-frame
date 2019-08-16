3e2eed063ac571b36e6983099208e19aeeb00b14
Tank <956461550@qq.com>
2019-08-16 18:47:16 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export interface IConfig {
+    port: number;
+    debugLogging: boolean;
+    dbsslconn: boolean;
+    jwtSecret: string;
+    databaseUrl: string;
+}
+
+const config: IConfig = {
+    port: +process.env.PORT || 3000,
+    debugLogging: process.env.NODE_ENV == 'development',
+    dbsslconn: process.env.NODE_ENV != 'development',
+    jwtSecret: process.env.JWT_SECRET || 'your-secret-whatever',
+    databaseUrl: process.env.DATABASE_URL || 'mysql://root:123456@localhost:3306/test'
+};
+
+export { config };
